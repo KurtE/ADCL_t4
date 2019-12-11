bc6d8ef6b25af86daa2bd32838f28e10544273c8
Kurt Eckhardt <kurte@rockisland.com>
2019-12-10 17:15:15 -0800

    Update DMA example plus sine wave for 2 DMAs running
    Updated test case app, to have simple class internal, to hold buffers and the like
    and Run two ADCs at the same time.
    
    Updated Sinewave app to allow two pots to change DAC1 and DAC0 differently

--- Examples/SineWav/SineWav.ino
+++ Examples/SineWav/SineWav.ino
@@ -1,15 +1,18 @@
 #include <Audio.h>
 #define POT_PIN 14
+#define POT_PIN2 15
 int last_pot_val = 0xffff;
+int last_pot_val2 = 0xffff;
 int led_on_time = 256;
 int led_off_time = 256;
 elapsedMillis led_timer;
 
 // GUItool: begin automatically generated code
 AudioSynthWaveform       waveform1;      //xy=110,75
+AudioSynthWaveform       waveform2;      //xy=110,75
 AudioOutputAnalogStereo  audioOutput;
 AudioConnection          patchCord1(waveform1, 0, audioOutput, 0);
-AudioConnection          patchCord2(waveform1, 0, audioOutput, 1);
+AudioConnection          patchCord2(waveform2, 0, audioOutput, 1);
 // GUItool: end automatically generated code
 
 void setup() {
@@ -20,6 +23,11 @@ void setup() {
   waveform1.begin(WAVEFORM_SINE);
   waveform1.frequency(60);
   waveform1.amplitude(0.99);
+
+  waveform2.begin(WAVEFORM_SINE);
+  waveform2.frequency(60);
+  waveform2.amplitude(0.99);
+
   analogReadResolution(12);  // 12 bit resolution
   led_timer = 0;
 }
@@ -33,14 +41,34 @@ void loop() {
     led_on_time = new_amp * 512;
     led_off_time = 512 - led_on_time;
     waveform1.amplitude(new_amp);
+    #ifndef POT_PIN2
+    waveform2.amplitude(new_amp);
+    #endif
     if (Serial) {
-      Serial.print("New Amp: ");
+      Serial.print("New Amp 1: ");
       Serial.print(new_amp, 4);
       Serial.print("(");
       Serial.print(pot_val, DEC);
       Serial.println(")");
     }
   }
+#ifdef POT_PIN2
+  pot_val = analogRead(POT_PIN2); // lets reduce noise
+  if (abs(pot_val - last_pot_val2) > 8) {
+    last_pot_val2 = pot_val;
+    float new_amp = (float)last_pot_val2 / 4096.0;
+    led_on_time = new_amp * 512;
+    led_off_time = 512 - led_on_time;
+    waveform2.amplitude(new_amp);
+    if (Serial) {
+      Serial.print("New Amp 2: ");
+      Serial.print(new_amp, 4);
+      Serial.print("(");
+      Serial.print(pot_val, DEC);
+      Serial.println(")");
+    }
+  }
+#endif  
   if (digitalReadFast(13)) {
     if (led_timer > led_on_time) {
       digitalWriteFast(13, LOW);
