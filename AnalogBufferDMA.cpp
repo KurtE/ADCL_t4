a3e7283495c54cb4c6f0ad1af391ba67448f21b4
Kurt Eckhardt <kurte@rockisland.com>
2019-12-13 13:52:41 -0800

    Move DMA buffer handling to external files/helper class
    Moved a lot of the ADC_DMA_RMS code into a semi independant class, which includes also the DMA ISR handling...
    
    Not sure if others might want the ability to muck directly with ISR, but...

---
+++ AnalogBufferDMA.cpp
@@ -0,0 +1,108 @@
+/* Teensy 3.x, LC, 4.0 ADC library
+ * https://github.com/pedvide/ADC
+ * Copyright (c) 2019 Pedro Villanueva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "AnalogBufferDMA.h"
+
+#define DEBUG_DUMP_DATA
+#ifdef DEBUG_DUMP_DATA
+extern void dumpDMA_TCD(DMABaseClass *dmabc);
+#endif
+// Global objects
+AnalogBufferDMA *AnalogBufferDMA::_activeObjectPerADC[2] = {nullptr, nullptr};
+
+
+
+void AnalogBufferDMA::init(ADCL *adc, int8_t adc_num)
+{
+    // enable DMA and interrupts
+    Serial.println("before enableDMA"); Serial.flush();
+
+
+    // setup a DMA Channel.
+    // Now lets see the different things that RingbufferDMA setup for us before
+
+    _dmasettings_adc[0].source((volatile uint16_t&)((adc_num==1)? ADC2_R0 : ADC1_R0));
+    _dmasettings_adc[0].destinationBuffer((uint16_t*)_buffer1, _buffer1_count*2); // 2*b_size is necessary for some reason
+    _dmasettings_adc[0].replaceSettingsOnCompletion(_dmasettings_adc[1]);    // go off and use second one... 
+    _dmasettings_adc[0].interruptAtCompletion(); //interruptAtHalf or interruptAtCompletion
+
+    _dmasettings_adc[1].source((volatile uint16_t&)((adc_num==1)? ADC2_R0 : ADC1_R0));
+    _dmasettings_adc[1].destinationBuffer((uint16_t*)_buffer2, _buffer2_count*2); // 2*b_size is necessary for some reason
+    _dmasettings_adc[1].replaceSettingsOnCompletion(_dmasettings_adc[0]);    // Cycle back to the first one
+    _dmasettings_adc[1].interruptAtCompletion(); //interruptAtHalf or interruptAtCompletion
+
+    _dmachannel_adc = _dmasettings_adc[0];
+
+    if (adc_num == 1) {
+    	_activeObjectPerADC[1] = this;
+	    _dmachannel_adc.attachInterrupt(&adc_1_dmaISR);
+	    _dmachannel_adc.triggerAtHardwareEvent(DMAMUX_SOURCE_ADC2); // start DMA channel when ADC finishes a conversion
+	} else {		
+    	_activeObjectPerADC[0] = this;
+	    _dmachannel_adc.attachInterrupt(&adc_0_dmaISR);
+	    _dmachannel_adc.triggerAtHardwareEvent(DMAMUX_SOURCE_ADC1); // start DMA channel when ADC finishes a conversion
+	}
+    //arm_dcache_flush((void*)dmaChannel, sizeof(dmaChannel));
+    _dmachannel_adc.enable();
+
+    adc->enableDMA(adc_num);    
+
+#ifdef DEBUG_DUMP_DATA
+    dumpDMA_TCD(&_dmachannel_adc);
+    dumpDMA_TCD(&_dmasettings_adc[0]);
+    dumpDMA_TCD(&_dmasettings_adc[1]);
+    if (adc_num == 1) {
+        Serial.printf("ADC2: HC0:%x HS:%x CFG:%x GC:%x GS:%x\n", ADC2_HC0, ADC2_HS,  ADC2_CFG, ADC2_GC, ADC2_GS);
+    } else {
+        Serial.printf("ADC1: HC0:%x HS:%x CFG:%x GC:%x GS:%x\n", ADC1_HC0, ADC1_HS,  ADC1_CFG, ADC1_GC, ADC1_GS);
+    }
+#endif
+
+    _last_isr_time = millis();
+}
+
+void  AnalogBufferDMA::processADC_DMAISR() {
+    digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
+    uint32_t cur_time = millis();
+
+    _interrupt_count++;
+    _interrupt_delta_time = cur_time - _last_isr_time;
+    _last_isr_time = cur_time;
+    // update the internal buffer positions
+    _dmachannel_adc.clearInterrupt();
+}
+
+void AnalogBufferDMA::adc_0_dmaISR() {
+	if (_activeObjectPerADC[0]) {
+		_activeObjectPerADC[0]->processADC_DMAISR();
+	}
+    asm("DSB");
+}
+
+void AnalogBufferDMA::adc_1_dmaISR() {
+	if (_activeObjectPerADC[1]) {
+		_activeObjectPerADC[1]->processADC_DMAISR();
+	}
+    asm("DSB");
+}
--- Examples/T4_ADC_DMA_RMS/T4_ADC_DMA_RMS.ino
+++ Examples/T4_ADC_DMA_RMS/T4_ADC_DMA_RMS.ino
@@ -4,6 +4,7 @@
 
 #include <ADCL_t4.h>
 #include <DMAChannel.h>
+#include <AnalogBufferDMA.h>
 
 const int readPin = 15;
 // optional to define second pin
@@ -15,31 +16,6 @@ extern void dumpDMA_TCD(DMABaseClass *dmabc);
 
 // Going to try two buffers here  using 2 dmaSettings and a DMAChannel
 
-// lets wrap some of our Dmasettings stuff into helper class
-class AnalogBufferDMA {
-    // keep our settings and the like:
-    DMASetting dmasettings_adc[2];
-    DMAChannel  dmachannel_adc;
-
-public: 
-    AnalogBufferDMA(volatile uint16_t *buffer1, uint16_t buffer1_count, volatile uint16_t *buffer2, uint16_t buffer2_count) :
-            _buffer1(buffer1), _buffer1_count(buffer1_count), _buffer2(buffer2), _buffer2_count(buffer2_count) {};
-    void init(ADCL *adc,  void (*dma_func)(), int8_t adc_num = -1);
-    inline void clearInterrupt() {dmachannel_adc.clearInterrupt();}
-    inline volatile uint16_t *bufferLastISRFilled() {return (interrupt_count & 1)? _buffer1 : _buffer2;}
-    inline uint16_t bufferCountLastISRFilled() {return (interrupt_count & 1)? _buffer1_count : _buffer2_count;}
-
-    volatile uint32_t interrupt_count = 0;
-    volatile uint32_t interrupt_delta_time;
-    volatile uint32_t last_isr_time;
-
-    volatile uint16_t *_buffer1;
-    uint16_t _buffer1_count;
-    volatile uint16_t *_buffer2;
-    uint16_t _buffer2_count;
-
-};
-
 const uint32_t buffer_size = 1600;
 DMAMEM static volatile uint16_t __attribute__((aligned(32))) dma_adc_buff1[buffer_size];
 DMAMEM static volatile uint16_t __attribute__((aligned(32))) dma_adc_buff2[buffer_size];
@@ -50,46 +26,6 @@ DMAMEM static volatile uint16_t __attribute__((aligned(32))) dma_adc2_buff1[buff
 DMAMEM static volatile uint16_t __attribute__((aligned(32))) dma_adc2_buff2[buffer_size];
 AnalogBufferDMA abdma2(dma_adc2_buff1, buffer_size, dma_adc2_buff2, buffer_size);
 #endif
-
-void AnalogBufferDMA::init(ADCL *adc, void (*dma_func)(), int8_t adc_num)
-{
-    // enable DMA and interrupts
-    Serial.println("before enableDMA"); Serial.flush();
-
-
-    // setup a DMA Channel.
-    // Now lets see the different things that RingbufferDMA setup for us before
-
-    dmasettings_adc[0].source((volatile uint16_t&)((adc_num==1)? ADC2_R0 : ADC1_R0));
-    dmasettings_adc[0].destinationBuffer((uint16_t*)_buffer1, _buffer1_count*2); // 2*b_size is necessary for some reason
-    dmasettings_adc[0].replaceSettingsOnCompletion(dmasettings_adc[1]);    // go off and use second one... 
-    dmasettings_adc[0].interruptAtCompletion(); //interruptAtHalf or interruptAtCompletion
-
-    dmasettings_adc[1].source((volatile uint16_t&)((adc_num==1)? ADC2_R0 : ADC1_R0));
-    dmasettings_adc[1].destinationBuffer((uint16_t*)_buffer2, _buffer2_count*2); // 2*b_size is necessary for some reason
-    dmasettings_adc[1].replaceSettingsOnCompletion(dmasettings_adc[0]);    // Cycle back to the first one
-    dmasettings_adc[1].interruptAtCompletion(); //interruptAtHalf or interruptAtCompletion
-
-    dmachannel_adc = dmasettings_adc[0];
-
-    dmachannel_adc.attachInterrupt(dma_func);
-    dmachannel_adc.triggerAtHardwareEvent((adc_num==1)? DMAMUX_SOURCE_ADC2 : DMAMUX_SOURCE_ADC1); // start DMA channel when ADC finishes a conversion
-    //arm_dcache_flush((void*)dmaChannel, sizeof(dmaChannel));
-    dmachannel_adc.enable();
-
-
-    dumpDMA_TCD(&dmachannel_adc);
-    dumpDMA_TCD(&dmasettings_adc[0]);
-    dumpDMA_TCD(&dmasettings_adc[1]);
-    if (adc_num == 1)
-        Serial.printf("ADC2: HC0:%x HS:%x CFG:%x GC:%x GS:%x\n", ADC2_HC0, ADC2_HS,  ADC2_CFG, ADC2_GC, ADC2_GS);
-    else
-        Serial.printf("ADC1: HC0:%x HS:%x CFG:%x GC:%x GS:%x\n", ADC1_HC0, ADC1_HS,  ADC1_CFG, ADC1_GC, ADC1_GS);
-
-
-    adc->enableDMA(adc_num);    
-    last_isr_time = millis();
-}
 
 void setup() {
     while (!Serial && millis() < 5000) ;
@@ -116,12 +52,12 @@ void setup() {
 
     // setup a DMA Channel.
     // Now lets see the different things that RingbufferDMA setup for us before
-    abdma1.init(adc, dmaBuffer_isr);
+    abdma1.init(adc, ADC_0);
 #ifdef PROCECESS_2_PINS
     Serial.println("Setup ADC_1");
     adc->setAveraging(8, ADC_1); // set number of averages
     adc->setResolution(12, ADC_1); // set bits of resolution
-    abdma2.init(adc, dmaBuffer_isr2, ADC_1);
+    abdma2.init(adc, ADC_1);
     adc->analogRead(readPin2, ADC_1);
 #endif
 
@@ -139,13 +75,13 @@ void loop() {
 
     // Maybe only when both have triggered?
 #ifdef PROCECESS_2_PINS
-    if ( abdma1.interrupt_delta_time && abdma2.interrupt_delta_time) {
-        if (abdma1.interrupt_delta_time) ProcessAnalogData(&abdma1, 0);
-        if (abdma2.interrupt_delta_time) ProcessAnalogData(&abdma2, 1);
+    if ( abdma1.interrupted() && abdma2.interrupted()) {
+        if (abdma1.interrupted()) ProcessAnalogData(&abdma1, 0);
+        if (abdma2.interrupted()) ProcessAnalogData(&abdma2, 1);
         Serial.println(); 
     }
 #else
-    if ( abdma1.interrupt_delta_time) {
+    if ( abdma1.interrupted()) {
         ProcessAnalogData(&abdma1, 0);
         Serial.println(); 
     }
@@ -174,35 +110,7 @@ void ProcessAnalogData(AnalogBufferDMA *pabdma, int8_t adc_num) {
     }
 
     int rms = sqrt(sum_delta_sq / buffer_size);
-    Serial.printf(" %d - %u(%u): %u <= %u <= %u %d ", adc_num, pabdma->interrupt_count, pabdma->interrupt_delta_time, min_val, sum_values / buffer_size, max_val, rms);
-     pabdma->interrupt_delta_time = 0;
-
+    Serial.printf(" %d - %u(%u): %u <= %u <= %u %d ", adc_num, pabdma->interruptCount(), pabdma->interruptDeltaTime(), min_val, 
+            sum_values / buffer_size, max_val, rms);
+     pabdma->clearInterrupt();
 }
-
-void dmaBuffer_isr() {
-    uint32_t cur_time = millis();
-
-//    digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
-    //Serial.printf("dmaBuffer_isr: %u\n", cur_time-last_isr_time);
-    abdma1.interrupt_count++;
-    abdma1.interrupt_delta_time = cur_time - abdma1.last_isr_time;
-    abdma1.last_isr_time = cur_time;
-    // update the internal buffer positions
-    abdma1.clearInterrupt();
-    asm("DSB");
-}
-
-#ifdef PROCECESS_2_PINS
-void dmaBuffer_isr2() {
-    uint32_t cur_time = millis();
-
-    digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
-    //Serial.printf("dmaBuffer_isr: %u\n", cur_time-last_isr_time);
-    abdma2.interrupt_count++;
-    abdma2.interrupt_delta_time = cur_time - abdma2.last_isr_time;
-    abdma2.last_isr_time = cur_time;
-    // update the internal buffer positions
-    abdma2.clearInterrupt();
-    asm("DSB");
-}
-#endif
